dc5bab82e7e3e789af0ba02a979171559f55fbaa
m_amigo <maria.lourdes.amigo@gmail.com>
2016-07-04 13:16:15 -0700

    test

--- WarehouseSystem/WarehouseSystem.cs
+++ WarehouseSystem/WarehouseSystem.cs
@@ -185,6 +185,7 @@ namespace WarehouseSystem
                 users.Focus();
             }
         }
+        //
 
         private void inventoryToolStripMenuItem_Click(object sender, EventArgs e)
         {
